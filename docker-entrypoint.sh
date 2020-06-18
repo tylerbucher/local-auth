b4f757a079fa63a2a85ff1de6c4fa5790a9d3d30
Tyler Bucher <tylerb318@gmail.com>
2020-06-18 17:42:04 -0500

    Fixed login issues.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 APP_URL=${HOST_URL}
 
 cp public/401.bak.html public/401.html
-sed -i "s/{HOST_URL}/${APP_URL}/g" public/401.html
+sed -i "s|{HOST_URL}|${APP_URL}|g" public/401.html
 
 echo "Starting application..."
 
